2c8cee8ba560f15bc45c8f22c80c051d5130839f
wkulikowski <wojciechkulikowski@icloud.com>
2021-10-25 14:51:42 +0200

    Add plausible analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="work-with-cercle.vercel.app">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
